9f991d24191d63b69db2768a1e1ee80e8b84b721
Keshav Gupta <guptakeshav0710@gmail.com>
2024-02-28 17:06:29 +0530

    user-model initialised

---
+++ Code/application/models.py
@@ -0,0 +1,21 @@
+from application.database import db
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True, auto_increment=True)
+    name=db.Column(db.String(75), nullable=False)
+    phone=db.Column(db.String(15), nullable=False, unique=True)
+    email=db.Column(db.String(75), nullable=False, unique=True)
+    password=db.Column(db.String(75), nullable=False)
+    address=db.Column(db.String(75), nullable=False)
+    role=db.Column(db.String(75), nullable=False, default='customer')
+
+    def __init__(self, name, phone, email, password, address, role):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.password = password
+        self.address = address
+        self.role = role
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
