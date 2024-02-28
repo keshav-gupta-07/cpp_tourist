a0c5c3d90ff30934584a339fa0a6c82226650b7d
Keshav Gupta <guptakeshav0710@gmail.com>
2024-02-28 17:20:45 +0530

    variables hardcode

--- Code/application/__init__.py
+++ Code/application/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 from application.database import db
+from application.models import *
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['secret_key'] = 'sdkvnksjn'
 app.config['sqlalchemy_database_url'] = 'sqlite:///database.sqlite3'
 
 db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 @app.route('/')
 def index():
     return render_template('index.html')
---
+++ Code/application/variables.py
@@ -0,0 +1,3 @@
+CUSTOMER='customer'
+ADMIN='admin'
+MAIN_ADMIN='main_admin'
